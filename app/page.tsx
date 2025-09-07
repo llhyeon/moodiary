741736c1ea4893d88934fb021ac18e9975c18a3e
llhyeon <0617mosses@gmail.com>
2025-09-07 14:56:07 +0900

    로그인 기능 개발 중

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
 import CLink from "~/components/common/CLink";
+import Logo from "~/components/common/Logo";
 
 export default function Home() {
   return (
     <section className="relative h-screen">
-      <figure className="flex justify-center mt-70">
-        <Image src="/logo/logo.svg" width={350} height={100} alt="moodiary" />
-      </figure>
+      <Logo className="mt-70" />
       <CLink
         className="absolute top-[300px] left-1/2 translate-x-[-50%] py-2 px-8 animate-float"
         href={"/login"}>
